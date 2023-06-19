77bc89d4dd505fa424ca373589e9f6d3eeb3205d
jirkavavrik <jiri.vavrik@protonmail.ch>
2023-06-19 17:38:20 +0200

    measurement interval increased to 5 minutes

--- src/arduino_code/arduino_code.ino
+++ src/arduino_code/arduino_code.ino
@@ -21,7 +21,7 @@ float h=0,t=0,p=0;
 int outage=0;
 
 unsigned long lastConnectionTime = 0L;       // last time you connected to the server, in milliseconds
-const unsigned long postingInterval = 150L * 1000L; // delay between updates, in milliseconds
+const unsigned long postingInterval = 300L * 1000L; // delay between updates, in milliseconds
 
 const char SSID[]     = SECRET_SSID;
 const char PASS[]     = SECRET_PASS;
